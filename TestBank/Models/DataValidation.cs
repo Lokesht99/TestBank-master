977bbf3d6e8650a60e82debe110ca828cb1c2aee
Lokesh99 <Lokesh99@jktech.com>
2022-05-26 11:57:58 +0530

    TestBank

---
+++ TestBank/Models/DataValidation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestBank.Models
+{
+    public class DataValidation:ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime CurrentDate = DateTime.Now;
+
+            string message = "";
+
+            if (Convert.ToDateTime(value) > CurrentDate)
+            {
+                message = "DOB should not be greater than current Date";
+                return new ValidationResult(message);
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
